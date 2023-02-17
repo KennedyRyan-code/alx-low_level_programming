93ce9e4a91d2320eae59da8126b6e785fe4cebaa
Kennedy <kenryan49@gmail.com>
2023-02-17 05:42:33 -0800

    Print Alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char x;
+
+	char i;
+
+	x = 'a';
+
+	i = 'A';
+	while
+		(x <= 'z') {
+			putchar(x);
+			x++;
+		}
+	while
+		(i <= 'Z') {
+			putchar(i);
+			i++;
+		}
+	putchar('\n');
+	return (0);
+}
