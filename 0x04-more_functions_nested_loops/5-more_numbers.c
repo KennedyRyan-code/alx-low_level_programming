6c56e070df306f37e2da5f1c2e75fd51d419ee6f
Kennedy <kenryan49@gmail.com>
2023-02-23 11:53:28 -0800

    Prints 10x

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * more_numbers - prinits 10 times the numbers, 1-14;
+ *
+ * Returns: void
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 1; j <= 14; j++)
+		{
+			_putchar('1');
+			_putchar(j % 10 + '0');
+		}
+	}
+	_putchar('\n');
+}
+
