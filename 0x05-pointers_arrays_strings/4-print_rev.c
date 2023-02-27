fcd37cd032a64ec17edc5c9e05dc8e4f74f93705
Kennedy <kenryan49@gmail.com>
2023-02-27 07:27:32 -0800

    Prints in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,14 +9,14 @@
 void print_rev(char *s)
 {
 	int i;
-	int n;
+
 
 	i = 0;
 
 	while (s[i] != '\0')
 		i++;
 
-	for (n = i - 1; n >= 0; n--)
+	for (i = i - 1; i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}
