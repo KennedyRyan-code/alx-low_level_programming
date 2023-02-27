ffa6a294102a517f48cd0ef294f26dbd0c950ec5
Kennedy <kenryan49@gmail.com>
2023-02-27 06:12:20 -0800

    Swaps intergers values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * swap_int - values of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	int x;
+
+	x = *a;
+	*a = *b;
+	*b = x;
+}
