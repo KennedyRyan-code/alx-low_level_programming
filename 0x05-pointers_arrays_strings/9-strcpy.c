f9b81bfe6b23eb70febb005de789ab47bedc09fb
Kennedy <kenryan49@gmail.com>
2023-02-27 10:12:23 -0800

    Copies the string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -3,15 +3,15 @@
  * _strcpy - copy the string pointed to by src to dest
  * @dest: char to check
  * @src: char to check
- * Return: 0 is success
+ * Return: 0 
  */
 
 char *_strcpy(char *dest, char *src)
 {
 	int a;
 
-	for (a = 0; src[a] != '0'; a++)
+	for (a = 0; src[a] != '\0'; a++)
 		dest[a] = src[a];
-	dest[a] = '\0';
+	dest[a++] = '\0';
 	return (dest);
 }
