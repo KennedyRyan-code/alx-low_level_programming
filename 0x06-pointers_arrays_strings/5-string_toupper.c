4c78f0f0cee538626a556a20aa9e8319c73cf630
Kennedy <kenryan49@gmail.com>
2023-03-01 03:38:12 -0800

    Changes all lower to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * string_toupper - changes lowercase to uppercase
+ * @s: the string
+ *
+ * Return: char
+ */
+
+char *string_toupper(char *s)
+{
+	int count = 0;
+
+	while (*(s + count))
+	{
+		if (*(s + count) >= 'a' && *(s + count) <= 'Z')
+			*(s + count) -= 'a' - 'A';
+		count++;
+	}
+	return (s);
+}
