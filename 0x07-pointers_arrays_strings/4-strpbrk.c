cc1cd73d86aeeda28a4612dabc989a9da4235d64
Kennedy <kenryan49@gmail.com>
2023-03-06 10:16:42 -0800

    Searches a string

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,12 +1,12 @@
 /**
  * _strpbrk - function that searches a string for any of a set of bytes
- *
  * @s:first occurrence in the string
- *
- * @accept: matches one of the bytes, or @NULL if no such byte
+ * @accept: matches one of the bytes,
+ * @NULL if no such byte
  *
  * Return: a pointer to the byte
  */
+
 char *_strpbrk(char *s, char *accept)
 {
 	int i;
@@ -15,9 +15,10 @@ char *_strpbrk(char *s, char *accept)
 			for (i = 0; accept[i]; i++)
 			{
 				if (*s == accept[i])
+					i++;
 					return (s);
 			}
-			s++;
+
 		}
 	return (NULL);
 }
