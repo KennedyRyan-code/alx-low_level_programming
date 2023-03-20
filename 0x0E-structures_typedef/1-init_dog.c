d8d33f4316005cf9879a2aa2426c3adfab2b3d1b
Kennedy <kenryan49@gmail.com>
2023-03-20 12:30:26 -0700

    initialize a variable

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -7,14 +7,14 @@
  * @age: age of the dog.
  * @owner: owner of the dog.
  *
- * Return: no return.
+ * Return: void.
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d)
 	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
+		(*d).name = name;
+		(*d).age = age;
+		(*d).owner = owner;
 	}
 }
