448ec89cbd394a9995911c4843c2a6dbf5c6165a
Kennedy <kenryan49@gmail.com>
2023-03-23 11:56:01 -0700

    integer search

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,19 +12,18 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
-	bool y;
 
-	if (array != NULL && size > 0 && cmp != NULL)
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
 	{
-		if (size <= 0)
-			return (-1);
-
-		for (i = 0; i < size; i++)
+		if (cmp(array[i]) != 0)
 		{
-			y = cmp(array[i]);
-			if (y == TRUE)
-				return (i);
+			return (i);
 		}
+
 	}
 	return (-1);
 
