aed386c1052ba1303b5d9ae056875626f32e5fd5
Kennedy <kenryan49@gmail.com>
2023-03-31 12:43:11 -0700

    adding a new node at the end of a list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -11,37 +11,32 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node = malloc(sizeof(list_t));
+	list_t *new_node, *last_node = *head;
 
+	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
-	{
 		return (NULL);
-	}
 
-	char *str_copy = strdup(str);
+	new_node->str = strdup(str);
 
-	if (str_copy == NULL)
+	if (new_node->str == NULL)
 	{
 		free(new_node);
 		return (NULL);
 	}
 
-	new_node->str = str_copy;
-	new_node->len = strlen(str_copy);
+	new_node->len = strlen(str);
+
 	new_node->next = NULL;
-
 	if (*head == NULL)
-		*head = new_node;
-	else
 	{
-		list_t *current = *head;
-
-		while (current->next != NULL)
-		{
-			current = current->next;
-		}
-		current->next = new_node; /* append new node to end of list */
+		*head = new_node;
+		return (new_node);
 	}
-	return (new_node);
+	while (last_node->next != NULL)
+		last_node = last_node->next;
 
+	last_node->next = new_node;
+
+	return (new_node);
 }
