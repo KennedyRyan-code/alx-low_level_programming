2a72c9a251246bfd1ff6abe186699cd71d89d93f
KennedyRyan-code <kenryan49@gmail.com>
2023-04-08 22:38:06 +0300

    Set bit at a given index

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,9 +10,13 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
+	unsigned long int mask;
+
 	if (index >= sizeof(unsigned long int) * 8)
 		return (-1); /* if index is out of range */
-	unsigned long int mask = 1UL << index;
+
+
+	mask = 1UL << index;
 
 	*n |= mask;
 	return (1);
