5eccc818a8ced2c01c5d2f6e6a2735b565d4a063
KennedyRyan-code <kenryan49@gmail.com>
2023-04-10 21:56:49 +0300

    0. Tread lightly, she is near

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,50 @@
+#include "main.h"
+/**
+ * read_textfile - reads a text file and prints it to the 'POSIX' STDOUT
+ * @filename: pointer veriable to the file
+ * @letters: number of letters to be read and printed
+ *
+ * Return: the number of letters it could read and print
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t bytes_read = 0;
+	char *buffer = NULL;
+	int file_descriptor = -1;
+
+	if (filename == NULL)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
+	file_descriptor = open(filename, O_RDONLY);
+	if (file_descriptor == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	bytes_read = read(file_descriptor, buffer, letters);
+	if (bytes_read == -1)
+	{
+		free(buffer);
+		close(file_descriptor);
+
+		return (0);
+	}
+
+	if (write(STDOUT_FILENO, buffer, bytes_read) != bytes_read)
+	{
+		free(buffer);
+		close(file_descriptor);
+
+		return (0);
+	}
+
+	free(buffer);
+	close(file_descriptor);
+	return (bytes_read);
+}
