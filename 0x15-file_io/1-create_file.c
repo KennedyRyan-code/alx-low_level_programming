41deb8f182e636d23343cf9c4c0f2519646c775a
KennedyRyan-code <kenryan49@gmail.com>
2023-04-12 07:41:59 +0300

    Create a function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,12 +20,12 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 	{
-		return -1;
+		return (-1);
 	}
 
 	file_descriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, file_mode);
 	if (file_descriptor == -1)
-		return -1;
+		return (-1);
 
 	if (text_content != NULL)
 	{
@@ -33,10 +33,10 @@ int create_file(const char *filename, char *text_content)
 		if (bytes_written == -1)
 		{
 			close(file_descriptor);
-			return -1;
+			return (-1);
 		}
 	}
 
 	close(file_descriptor);
-	return 1;
+	return (1);
 }
