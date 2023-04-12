63ca50c67b745cbccd31b40cf1ea99df71e7df23
KennedyRyan-code <kenryan49@gmail.com>
2023-04-12 19:14:00 +0300

    0. Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,6 +1,3 @@
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
 #include "main.h"
 /**
  * read_textfile - reads a text file and prints it to the 'POSIX' STDOUT
@@ -12,19 +9,19 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t bytes_read = 0;
+	ssize_t bytes_read, bytes_write;
 	char *buffer = NULL;
 	int file_descriptor;
 
 	if (filename == NULL)
 		return (0);
 
-	buffer = malloc(sizeof(char *) * letters);
+	buffer = malloc(sizeof(char) * letters);
 
 	if (buffer == NULL)
 		return (0);
 
-	file_descriptor = open(filename, O_RDONLY, 0600);
+	file_descriptor = open(filename, O_RDONLY);
 	if (file_descriptor == -1)
 	{
 		free(buffer);
@@ -32,18 +29,11 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	}
 
 	bytes_read = read(file_descriptor, buffer, letters);
-	if (bytes_read == -1)
-	{
-		free(buffer);
-		close(file_descriptor);
 
-		return (0);
-	}
+	bytes_write = write(STDOUT_FILENO, buffer, bytes_read);
 
-	write(STDOUT_FILENO, buffer, bytes_read);
-
-		free(buffer);
-		close(file_descriptor);
+	free(buffer);
+	close(file_descriptor);
 
 
 	return (bytes_read);
