095f94ef65ab0271c3d5d86f85f4b26595a5442d
KennedyRyan-code <kenryan49@gmail.com>
2023-08-23 20:24:38 +0300

    Island Perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Module to calculate the perimeter of an island."""
+
+def island_perimeter(grid):
+    """
+    Calculates the perimeter of an island described in the grid.
+
+    Args:
+    grid (list[list[int]]): A list of lists representing the island grid.
+
+    Returns:
+    int: The perimeter of the island.
+    """
+    perimeter = 0
+    rows = len(grid)
+    cols = len(grid[0])
+
+    for row in range(rows):
+        for col in range(cols):
+            if grid[row][col] == 1:
+                perimeter += 4
+
+                if row > 0 and grid[row - 1][col] == 1:
+                    perimeter -= 2  # Subtract 2 for adjacent land cells
+                if col > 0 and grid[row][col - 1] == 1:
+                    perimeter -= 2  # Subtract 2 for adjacent land cells
+
+    return perimeter
